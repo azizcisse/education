8f2a12623df02e949e5cc5859a73188ad904ba95
azizcisse <alaziz6c78@gmail.com>
2023-02-07 17:55:21 +0000

    first commit

---
+++ lib/widgets/description_section.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class DescriptionSection extends StatelessWidget {
+  const DescriptionSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 20),
+      child: Column(
+        children: [
+          Text(
+            "On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte.",
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.black.withOpacity(0.7),
+            ),
+            textAlign: TextAlign.justify,
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              Text(
+                "Durée du Cours: ",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Icon(
+                Icons.timer,
+                color: Color(0xFF674AEF),
+              ),
+              SizedBox(width: 5),
+              Text(
+                "26 Heures",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              Text(
+                "Evaluation: ",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Icon(
+                Icons.star,
+                color: Colors.amber,
+              ),
+              SizedBox(width: 5),
+              Text(
+                "4.5",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
